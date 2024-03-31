4b4ac7a290e38693dc9138997e631590a98ffa12
owenEEfobi <jenukohaefobi@gmail.com>
2024-04-01 00:05:59 +0100

    Update next.config.mjs to include external /blog

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://starter-kit-brown-two.vercel.app/blog",
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://starter-kit-brown-two.vercel.app/blog/:path*", 
+      },
+    ];
+  },
+};
 
 export default nextConfig;
