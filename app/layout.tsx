100753d9205dbc972dcc28fc468a80b193133e76
Jefob1 <jenukohaefobi@gmail.com>
2024-05-13 17:41:43 +0100

    build(deps): update layout.tsx, package.json and yarn.lock

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type {Metadata} from "next";
 import {Lato as FontSans} from "next/font/google";
+import {Analytics} from "@vercel/analytics/react";
 import {LanguageContextProvider, ThemeProvider} from "@/lib/providers";
 import {Toaster} from "@/components/ui/sonner";
 import Header from "@/components/Header";
@@ -14,14 +15,15 @@ const fontSans = FontSans({
 
 export const metadata: Metadata = {
   title: {
-    template: '%s | Owenstack',
-    default:'Owenstack'
+    template: "%s | Owenstack",
+    default: "Owenstack",
   },
-  description: "Full-stack web developer that builds beautiful and functional websites",
-  metadataBase: new URL('https://owenstack.github.io'),
+  description:
+    "Full-stack web developer that builds beautiful and functional websites",
+  metadataBase: new URL("https://owenstack.github.io"),
   openGraph: {
-    images: '/og-image.jpg'
-  }
+    images: "/og-image.jpg",
+  },
 };
 
 export default function RootLayout({
@@ -44,6 +46,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+            <Analytics />
             <Header />
             {children}
           </ThemeProvider>
